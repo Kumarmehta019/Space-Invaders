115a64fb73c2fe68053a81796df04ab962a55d00
Kumar Mehta <kumar@Kumars-iMac.local>
2021-10-08 15:49:04 +0100

    adding keys and legends to the game introduction screen

--- app.js
+++ app.js
@@ -2,9 +2,25 @@ function init() {
 
 // creating Grid for key-legend wrapper within the game intro wrapper- I needed to create six divs in width and 2 in height //
 
+//   const grid = document.querySelector(".key-legend-grid")
 
+//   const keyLegendGridWidth = 6
+//   const keyLegendGridHeight = 2
+//   const keyLegendGridCellCount = keyLegendGridWidth * keyLegendGridHeight
+//   const keyLegendGridCells = []
 
+//   function createGrid() {
+//     for (let i = 0; i < keyLegendGridCellCount; i++) {
+//       const keyLegendGridCell = document.createElement('div')
+//       keyLegendGridCell.innerText = i 
+//       grid.appendChild(keyLegendGridCell)
+//       keyLegendGridCells.push(keyLegendGridCell)
+//     }
+//   }
 
+//   createGrid()
+
+//   console.log(keyLegendGridCells)
 
 
 
