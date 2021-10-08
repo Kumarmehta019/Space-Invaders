5eccce030b5988abd9b988620bb906f343400091
Kumar Mehta <kumar@Kumars-iMac.local>
2021-10-08 23:01:27 +0100

    adding JS functionality by creating gride and adding one of each alien and spaceship on the grid

--- app.js
+++ app.js
@@ -1,26 +1,57 @@
 function init() {
 
-// creating Grid for key-legend wrapper within the game intro wrapper- I needed to create six divs in width and 2 in height //
+// creating Grid for the game & starting positions of the three aliens//
 
-//   const grid = document.querySelector(".key-legend-grid")
+  const grid = document.querySelector('.grid')
+  const width = 11
+  const height = 20
+  const cellCount = width * height
+  const cells = []
 
-//   const keyLegendGridWidth = 6
-//   const keyLegendGridHeight = 2
-//   const keyLegendGridCellCount = keyLegendGridWidth * keyLegendGridHeight
-//   const keyLegendGridCells = []
+  
+  const startingPinkyPosition = 0 
+  const startingBlueyPosition = 22
+  const startingGreenyPosition = 44
+  let currentPinkyPosition = 0
+  let currentBlueyPosition = 0
+  let currentGreenyPosition = 0
+  const spaceshipStartingPosition = 214
+  let currentSpaceshipPosition = 0
+  const pinkyClass = 'pinky'
+  const blueyClass = 'bluey'
+  const greenyClass = 'greeny'
+  const spaceshipClass = 'spaceship'
 
-//   function createGrid() {
-//     for (let i = 0; i < keyLegendGridCellCount; i++) {
-//       const keyLegendGridCell = document.createElement('div')
-//       keyLegendGridCell.innerText = i 
-//       grid.appendChild(keyLegendGridCell)
-//       keyLegendGridCells.push(keyLegendGridCell)
-//     }
-//   }
+  function createGrid() {
+    for (let i = 0; i < cellCount; i++) {
+      const cell = document.createElement('div')
+      cell.innerText = i 
+      grid.appendChild(cell)
+      cells.push(cell)
+    }
+    addPinky(0)
+    addBluey([22])
+    addGreeny([44])
+    addSpaceship([214])
+  }
+  
+  console.log()
+  function addPinky(position) {
+    cells[position].classList.add(pinkyClass)
+  }
+  function addBluey(position) {
+    cells[position].classList.add(blueyClass)
+  }
+  function addGreeny(position) {
+    cells[position].classList.add(greenyClass)
+  }
+  function addSpaceship(position) {
+    cells[position].classList.add(spaceshipClass)
+  }
 
-//   createGrid()
+  createGrid()
 
-//   console.log(keyLegendGridCells)
+
 
 
 
